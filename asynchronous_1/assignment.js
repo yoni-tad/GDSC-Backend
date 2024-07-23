e24a16f600b17c224a51806e80dbc4c4dbc6275e
yoni-tad <yonitad2404@gmail.com>
2024-07-23 17:27:54 +0300

    update

--- asynchronous_1/assignment.js
+++ asynchronous_1/assignment.js
@@ -1,6 +1,30 @@
 async function fetcher() {
-    let response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+  try {
+    let response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
     let final = await response.json();
     console.log(final);
+  } catch (error) {
+    console.log("Error fetching");
+  }
 }
-fetcher();
+// fetcher();
+
+function fetcher1() {
+    return new Promise((resolve, reject) => {
+        let promise = fetch('https://jsonplaceholder.typicode.com/todos/1');
+        promise.then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+        }) 
+        .then(data => {
+            resolve(data);
+            console.log(data);
+        })
+        .catch(error => {
+            reject(error);
+        })
+    });
+}
+
+fetcher1();
