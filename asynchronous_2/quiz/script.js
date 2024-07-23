16e7c31237fb92c9eac491ab3ddfe0f2417aa501
yoni-tad <yonitad2404@gmail.com>
2024-07-23 17:09:14 +0300

    update 07/23

--- asynchronous_2/quiz/script.js
+++ asynchronous_2/quiz/script.js
@@ -4,12 +4,12 @@ var isClicked = false;
 buttonContainer.addEventListener("click", (event) => {
   let targeted = event.target;
   isClicked = !isClicked;
-  let currentText = targeted.innerText;
-  console.log(currentText);
+
+  console.log(event);
 
   if (isClicked) {
     targeted.textContent = "Clicked!";
   } else {
-    targeted.textContent = currentText;
+    targeted.textContent = targeted.innerText;
   }
 });
