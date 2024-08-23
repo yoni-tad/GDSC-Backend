40bc725055b46dd4ccc0e7c8b643841f8084c22f
yoni-tad <yonitad2404@gmail.com>
2024-08-23 17:09:59 +0300

    update 08/23

--- project/blog/models/article.js
+++ project/blog/models/article.js
@@ -1,4 +1,9 @@
 const mongoose = require("mongoose");
+const { marked } = require("marked");
+const slugify = require("slugify");
+const createDomPurify = require("dompurify");
+const { JSDOM } = require("jsdom");
+const domPurify = createDomPurify(new JSDOM().window);
 
 const articlesSchema = new mongoose.Schema({
   title: {
@@ -12,10 +17,31 @@ const articlesSchema = new mongoose.Schema({
     type: String,
     require: true,
   },
-  createAt: {
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  sanitizedHtml: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
     type: Date,
     default: Date.now,
   },
+});
+
+articlesSchema.pre("validate", function () {
+  if (this.title) {
+    this.slug = slugify(this.title, { lower: true, strict: true });
+  }
+
+  if (this.markdown) {
+    this.sanitizedHtml = domPurify.sanitize(marked(this.markdown))
+  }
+
+  // next()
 });
 
 module.exports = mongoose.model("Article", articlesSchema);
--- project/blog/server.js
+++ project/blog/server.js
@@ -1,27 +1,18 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const articleRouter = require("./routes/articles");
+const express = require("express")
+const mongoose = require("mongoose")
+const Article = require("./models/article")
+const articleRouter = require("./routes/articles")
+const methodOverride = require("method-override")
 const app = express();
 
 mongoose.connect("mongodb://localhost/blog", {});
 
 app.set("view engine", "ejs");
-
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'))
 
-app.get("/", (req, res) => {
-  const articles = [
-    {
-      title: "Test article",
-      createdAt: new Date(),
-      description: "Test description",
-    },
-    {
-      title: "Test article",
-      createdAt: new Date(),
-      description: "Test description",
-    },
-  ];
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({createdAt: 'desc'})
   res.render("articles/index", { articles: articles });
 });
 
