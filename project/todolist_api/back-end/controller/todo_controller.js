f6926f9b5a72a3165fdf47b77ab6c787a4ddf729
yoni-tad <yonitad2404@gmail.com>
2024-08-25 01:42:17 +0300

    update

---
+++ project/todolist_api/back-end/controller/todo_controller.js
@@ -0,0 +1,55 @@
+const Todo = require('../model/todos')
+
+exports.getTodoController = async (req, res) => {
+    try {
+        const todos = await Todo.find({}, 'todo checked')
+        if (!todos) {
+            return res.status(404).json({message: 'Todos not found'})
+        }
+        res.json(todos)
+    } catch(e) {
+        console.log(e.message)
+        res.status(500).json({message: 'Server error'})
+    }
+}
+
+exports.postTodoController = async (req, res) => {
+    try {
+        const newTodo = Todo.create({
+            todo: req.body.todo
+        })
+
+        res.status(201).json(newTodo)
+    } catch(e) {
+        console.log(e.message)
+        res.status(500).json({message: 'Server error'})
+    }
+}
+
+exports.putTodoController = async (req, res) => {
+    try {
+        const todo = await Todo.findById(req.params.id)
+        if (!todo) {
+            res.status(404).json({message: "Todo not found"})
+        }
+        const updateTodo = await Todo.findByIdAndUpdate(req.params.id, {checked: req.body.checked}, {new: true})
+        res.json(updateTodo)
+    } catch(e) {
+        console.log(e.message)
+        res.status(500).json({message: 'Server error'})
+    }
+}
+
+exports.deleteTodoController = async(req, res) => {
+    try {
+        const todo = await Todo.findById(req.params.id)
+        if (!todo) {
+            res.status(404).json({message: "Todo not found"})
+        }
+        const deleteTodo = await Todo.findByIdAndDelete(req.params.id)
+        res.json({message: "Todo delete successfully"})
+    } catch(e) {
+        console.log(e.message)
+        res.status(500).json({message: "Server error"})
+    }
+}
--- project/todolist_api/back-end/router/api.js
+++ project/todolist_api/back-end/router/api.js
@@ -1,57 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const Todo = require('../model/todos')
+const { postTodoController, getTodoController, putTodoController, deleteTodoController } = require('../controller/todo_controller')
 
-router.get("/todos", async (req, res) => {
-    try {
-        const todos = await Todo.find({}, 'todo checked')
-        if (!todos) {
-            return res.status(404).json({message: 'Todos not found'})
-        }
-        res.json(todos)
-    } catch(e) {
-        console.log(e.message)
-        res.status(500).json({message: 'Server error'})
-    }
-})
+router.get("/todos", getTodoController)
 
-router.post("/todos", async (req, res) => {
-    try {
-        const newTodo = new Todo({
-            todo: req.body.todo
-        })
-        const saveTodo = await newTodo.save()
-        res.status(201).json(saveTodo)
-    } catch(e) {
-        console.log(e.message)
-        res.status(500).json({message: 'Server error'})
-    }
-})
+router.post("/todos", postTodoController)
 
-router.put('/todos/:id', async (req, res) => {
-    try {
-        const updateTodo = await Todo.findByIdAndUpdate(req.params.id, {checked: req.body.checked}, {new: true})
-        if(!updateTodo) {
-            return res.status(404).json({message: "Todo not found"})
-        }
-        res.json(updateTodo)
-    } catch(e) {
-        console.log(e.message)
-        res.status(500).json({message: 'Server error'})
-    }
-})
+router.put('/todos/:id', putTodoController)
 
-router.delete('/todos/:id', async(req, res) => {
-    try {
-        const deleteTodo = await Todo.findByIdAndDelete(req.params.id)
-        if(!deleteTodo){
-            return res.status(404).json({message: "Todo not found"})
-        }
-        res.json({message: "Todo delete successfully"})
-    } catch(e) {
-        console.log(e.message)
-        res.status(500).json({message: "Server error"})
-    }
-})
+router.delete('/todos/:id', deleteTodoController)
 
 module.exports = router
