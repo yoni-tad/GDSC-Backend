9c2db60565075750f03d4091370a6edf8a0feb6c
yoni-tad <yonitad2404@gmail.com>
2024-08-26 08:57:48 +0300

    update 08/26

--- project/todolist_api/back-end/server.js
+++ project/todolist_api/back-end/server.js
@@ -1,20 +1,29 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const cors = require("cors")
-const router = require("./router/api")
-const app = express()
-
-mongoose.connect('mongodb+srv://yonitube2404:beti2020yoni@todo-list.ynr3p.mongodb.net/?retryWrites=true&w=majority&appName=todo-list')
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const env = require("dotenv");
+const router = require("./router/api");
+const app = express();
+env.config();
 
 const coreOptions = {
-    origin: "*",
-    Credential: true,
-    optionSuccessStatus: 200
-}
+  origin: "*",
+  Credential: true,
+  optionSuccessStatus: 200,
+};
 
-app.use(cors(coreOptions))
-app.use(express.json())
-app.use('/api', router)
-app.use(express.static('../front-end'))
+app.use(cors(coreOptions));
+app.use(express.json());
+app.use("/api", router);
+app.use(express.static("../front-end"));
 
-app.listen(3000, () => console.log("Server start port at 3000"))
+(async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ MongoDB connected successfully.");
+
+    await app.listen(3000, () => console.log("✅ Server start port at 3000"));
+  } catch (e) {
+    console.error("❌ Server error:", e.message);
+  }
+})();
